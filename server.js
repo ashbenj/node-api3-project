cea75c907d9884cae8e4fcb59713a295535f5687
Asha <ashalbenjamin@gmail.com>
2020-05-14 14:42:16 -0500

    adding dependencies, setting up index and server files

--- server.js
+++ server.js
@@ -1,9 +1,29 @@
 const express = require('express');
+const helmet = require('helmet');
+const morgan = require('morgan');
+
+const postRouter = require('./posts/postRouter');
+const userRouter = require('./users/userRouter');
 
 const server = express();
 
+server.use(express.json());
+server.use(helmet());
+server.use(morgan('dev'));
+server.use((req, res, next) => {
+	console.log(req.method);
+	next();
+});
+
+server.use('/users', userRouter);
+server.use('/posts', postRouter);
+
 server.get('/', (req, res) => {
-  res.send(`<h2>Let's write some middleware!</h2>`);
+	const nameInsert = req.name ? ` ${req.name}` : '';
+
+	res.send(`
+  <h2>Lambda Hubs API</h2>
+  <p>Welcome${nameInsert} to the Lambda Hubs API`);
 });
 
 //custom middleware
